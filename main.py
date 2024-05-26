5fc3361019b45d0e74849a90745b62d5f8b0c1ac
dhrubaseal <dhrubaseal@gmail.com>
2024-05-26 17:34:25 +0530

    version_5

--- main.py
+++ main.py
@@ -34,31 +34,19 @@ def main():
             tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
     # Assign Ball Aquisition
-    player_assigner = PlayerBallAssigner()
-
-    team_ball_control = []
-
+    player_assigner =PlayerBallAssigner()
+    team_ball_control= []
     for frame_num, player_track in enumerate(tracks['players']):
         ball_bbox = tracks['ball'][frame_num][1]['bbox']
         assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
 
-    if assigned_player != -1:
-        tracks['players'][frame_num][assigned_player]['has_ball'] = True
-        team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
-    else:
-        if team_ball_control:
-            team_ball_control.append(team_ball_control[-1])
+        if assigned_player != -1:
+            tracks['players'][frame_num][assigned_player]['has_ball'] = True
+            team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
         else:
-            team_ball_control.append('None')
-
-        # if assigned_player != -1:
-        #     tracks['players'][frame_num][assigned_player]['has_ball'] = True
-        #     team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
-        # else:
-        #     team_ball_control.append(team_ball_control[-1])
-
-    team = np.array(team_ball_control)
-
+            team_ball_control.append(team_ball_control[-1])
+    team_ball_control= np.array(team_ball_control)
+    
     # # Save croppped image of player
 
     # for track_id, player in tracks['players'][0].items():
